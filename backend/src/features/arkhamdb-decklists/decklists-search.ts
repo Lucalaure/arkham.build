29938dfb887099a61a8d7589afcf496021e45fa2
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2026-01-02 07:36:05 +0100

    fix: always rank decks with >= 5 likes above decks with fewer than 5

--- backend/src/features/arkhamdb-decklists/decklists-search.ts
+++ backend/src/features/arkhamdb-decklists/decklists-search.ts
@@ -129,7 +129,7 @@ export async function search(db: Database, search: DecklistSearchRequest) {
         "arkhamdb_user.reputation as user_reputation",
         "investigator.faction_code as investigator_faction_code",
         sql<number>`
-          CASE WHEN arkhamdb_decklist.like_count >= 10 THEN 1 ELSE 0 END +
+          CASE WHEN arkhamdb_decklist.like_count >= 5 THEN 1 ELSE 0 END +
           (LN(arkhamdb_decklist.like_count + 1) / LN(arkhamdb_ranking_cache.max_like_count + 1)) * 0.6 +
           EXP(-0.006 * GREATEST(0, EXTRACT(EPOCH FROM (CURRENT_DATE - arkhamdb_decklist.date_creation)) / 86400 - 30)) * 0.4
         `.as("ranking_score"),
