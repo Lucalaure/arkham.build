c28f56990a3cb64c20dc17d61e618cd06b81248b
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2026-01-13 21:48:14 +0100

    refactor: use node's pipeline stream to speed up csv processing

--- backend/src/scripts/ingest-arkhamdb-decklists.ts
+++ backend/src/scripts/ingest-arkhamdb-decklists.ts
@@ -5,6 +5,7 @@ import { createReadStream, createWriteStream } from "node:fs";
 import { unlink } from "node:fs/promises";
 import { tmpdir } from "node:os";
 import { join } from "node:path";
+import { pipeline } from "node:stream/promises";
 import { parse } from "@fast-csv/parse";
 import type { Insertable, Transaction } from "kysely";
 import { connectionString, type Database, getDatabase } from "../db/db.ts";
@@ -266,31 +267,12 @@ async function downloadCsvFile(config: Config, name: string): Promise<string> {
   const res = await fetch(
     `${config.INGEST_URL_ARKHAMDB_DECKLISTS}/${name}.csv`,
   );
-
   if (!res.ok) throw new Error(`Failed to fetch ${name}: ${res.statusText}`);
 
+  assert(res.body, "Response body is null");
+
   const tempFilePath = join(tmpdir(), `${name}-${Date.now()}.csv`);
-  const writeStream = createWriteStream(tempFilePath);
-
-  await new Promise((resolve, reject) => {
-    assert(res.body, `Response body is null for ${name}`);
-
-    res.body.pipeTo(
-      new WritableStream({
-        write(chunk) {
-          writeStream.write(chunk);
-        },
-        close() {
-          writeStream.end();
-          resolve(undefined);
-        },
-        abort(error) {
-          writeStream.destroy();
-          reject(error);
-        },
-      }),
-    );
-  });
+  await pipeline(res.body, createWriteStream(tempFilePath));
 
   return tempFilePath;
 }
